3e6abd41f2d0e62fa1b698086b527500a08fe120
princekumar <princepk7878kumar@gmail.com>
2025-05-10 04:24:18 +0530

    done

--- app/Http/Controllers/routeController.php
+++ app/Http/Controllers/routeController.php
@@ -5,7 +5,39 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class routeController extends Controller
+class RouteController extends Controller
 {
-    //
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+    public function services()
+    {
+        return view('service');
+    }
+    public function blog()
+    {
+        return view('blog');
+    }
+    public function blogDetails($id)
+    {
+        return view('blog-details', ['id' => $id]);
+    }
+    public function project()
+    {
+        return view('project');
+    }
+    public function projectDetails($id)
+    {
+        return view('project-details', ['id' => $id]);
+    }
+  
+    public function home()
+    {
+        return view('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RouteController::class,'home'])->name('home');
+
+Route::get('/about',[RouteController::class,'about'])->name('about');
+Route::get('/contact',[RouteController::class,'contact'])->name('contact');
+Route::get('/service',[RouteController::class,'services'])->name('service');
+Route::get('/blog',[RouteController::class,'blog'])->name('blog');
+Route::get('/blog/{id}',[RouteController::class,'blogDetails'])->name('blog.details');
+Route::get('/project',[RouteController::class,'project'])->name('project');
+Route::get('/project/{id}',[RouteController::class,'projectDetails'])->name('project.details');
+
